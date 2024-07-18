6d5ed6b88c21dcea4b11a430d9f01b816571cbfd
Olakunle Kuye <olakuye@gmail.com>
2024-07-18 13:53:50 +0100

    Initial commit

---
+++ src/main/java/com/example/technical_assignment_be/TechnicalAssignmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.technical_assignment_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalAssignmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalAssignmentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/technical_assignment_be/TechnicalAssignmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technical_assignment_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAssignmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
